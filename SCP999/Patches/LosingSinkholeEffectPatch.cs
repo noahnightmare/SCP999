c8d3804a5c97225b74740a30d102894c5b298850
noah <41436356+noahnightmare@users.noreply.github.com>
2024-03-28 22:59:54 +0000

    Add a patch that stops the player from losing the sinkhole effect when walking over a sinkhole

---
+++ SCP999/Patches/LosingSinkholeEffectPatch.cs
@@ -0,0 +1,34 @@
+﻿namespace SCP999.Patches
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using CustomPlayerEffects;
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using HarmonyLib;
+    using Exiled.CustomRoles.API.Features;
+    using global::SCP999.Role;
+    using Exiled.API.Extensions;
+
+    [HarmonyPatch(typeof(StatusEffectBase), nameof(StatusEffectBase.ServerDisable))]
+    public class LosingSinkholeEffectPatch
+    {
+        // patches losing the sinkhole effect when trying to lose an effect as 999
+        public static bool Prefix(StatusEffectBase __instance)
+        {
+            if (!Player.TryGet(__instance.gameObject, out Player player))
+                return true;
+
+            if (!CustomRole.Get(typeof(CustomRoleScp999)).Check(player))
+                return true;
+
+            if (!__instance.TryGetEffectType(out EffectType effect))
+                return true;
+
+            if (effect != EffectType.SinkHole) 
+                return true;
+
+            return false;
+        }
+    }
+}
